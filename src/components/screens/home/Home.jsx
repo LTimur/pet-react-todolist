a53c650485a08d975b6ed349e9dfb579d238f5f3
LTimur <ledenev-timur@yandex.ru>
2023-04-17 18:24:27 +0500

    add dark theme

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoItem from "./item/TodoItem";
 import CreateTodoField from "./create-todo-field/CreateTodoField";
+import { BsMoonFill } from "react-icons/bs";
 
 const data = []
 
 const Home = () => {
+	// Dark theme feature
+	const [theme, setTheme] = useState('light')
+
+	useEffect(() => {
+	  if(theme === 'dark') {
+		document.documentElement.classList.add('dark');
+	  } else {
+		document.documentElement.classList.remove('dark');
+	  }
+	}, [theme])
+	
+	const handleThemeSwitch = () => {
+		setTheme(theme === 'dark' ? 'light': 'dark')
+	}
 
 	// Todos features
 	const [todos, setTodos] = useState(data)
@@ -21,7 +36,12 @@ const Home = () => {
 	return (
 		<div className='text-white w-4/5 mx-auto'>
 			<h1 className='text-2xl font-bold text-center dark:text-white text-slate-900'>Todolist</h1>
-			
+			<button onClick={handleThemeSwitch} className='float-right pr-10'>
+				<BsMoonFill
+					size={22}
+					className='text-gray-500 hover:text-yellow-300 transition-colors ease-in-out duration-300 '
+				/>
+			</button>
 			<CreateTodoField setTodos={setTodos} />
 			{todos.map(todo => (
 				<TodoItem
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/screens/home/Home';
+import Layout from './components/layout/Layout';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Layout>
+      <Home />
+    </Layout>
   </React.StrictMode>
 );
 
